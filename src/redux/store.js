1d2cb6e9083d800b5b9e48b4cffe25b722726ba1
NazarLaptop <nazar91187@gmail.com>
2022-04-20 11:58:07 +0300

    init structure

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+
+const rootReducer = combineReducers({
+
+})
+const store = configureStore({
+    reducer: rootReducer
+})
+export default store
