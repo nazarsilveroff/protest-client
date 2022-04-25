9607e26ad6409610776fdd35e662f2f81ef7d0b5
Svitlana <87758266+Sv0707@users.noreply.github.com>
2022-04-25 19:31:14 +0300

    header&modal done

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,14 +1,84 @@
-import React from 'react';
+import s from "./Header.module.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import HeaderMenu from "./HeaderMenu/HeaderMenu";
+import NavMenu from "./NavMenu/NavMenu";
+import Modal from "../Modal/Modal";
 
+function Header() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const breakPointTablet = 768;
+  const userName = "Dmitri";
 
+  const handleResizeWindow = () => setWidth(window.innerWidth);
 
-function Header(props) {
+  useEffect(() => {
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
 
-    return (
-        <nav>
-        </nav>
-    )
+  return (
+    <>
+      <header className={s.header}>
+        <Link to={"/"}>
+          <img src={logo} alt={"logo"} className={s.logo} />
+        </Link>
+        {width < breakPointTablet ? (
+          <div className={s.userInfo}>
+            {isLoggedIn && (
+              <div className={s.userName_circle}>{userName.slice(0, 1)}</div>
+            )}
+
+            <div className={s.menu}>
+              <GiHamburgerMenu size={15} onClick={() => setIsModalOpen(true)} />
+            </div>
+          </div>
+        ) : (
+          <>
+            <NavMenu isLoggedIn={isLoggedIn} />
+            {isLoggedIn && (
+              <div className={s.userInfo}>
+                <div className={s.userName_circle}>{userName.slice(0, 1)}</div>
+                <div className={s.userName}>{userName}</div>
+                <div className={s.signOut}>
+                  <RiLogoutBoxRLine
+                    size={16}
+                    onClick={() => setIsLoggedIn(false)}
+                  />
+                </div>
+              </div>
+            )}
+          </>
+        )}
+      </header>
+
+      {isModalOpen && (
+        <Modal
+          onClose={() => {
+            setIsModalOpen(false);
+          }}
+        >
+          <HeaderMenu
+            isLoggedIn={isLoggedIn}
+            onLogOut={() => {
+              setIsLoggedIn(false);
+            }}
+            onModalClose={() => {
+              setIsModalOpen(false);
+            }}
+          />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 export default Header;
---
+++ src/modules/Header/NavMenu/NavMenu.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import s from "./NavMenu.module.css";
+
+function NavMenu({ isLoggedIn }) {
+  return (
+    <nav className={s.nav}>
+      <ul className={s.list}>
+        {isLoggedIn && (
+          <li className={s.item}>
+            <NavLink
+              to="/"
+              exact
+              className={(navData) =>
+                navData.isActive ? s.activeLink : s.link
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li className={s.item}>
+            <NavLink
+              to="/materials"
+              exact
+              className={(navData) =>
+                navData.isActive ? s.activeLink : s.link
+              }
+            >
+              Materials
+            </NavLink>
+          </li>
+        )}
+        <li className={s.item}>
+          <NavLink
+            to="/contacts"
+            exact
+            className={(navData) => (navData.isActive ? s.activeLink : s.link)}
+          >
+            Contacts
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+NavMenu.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default NavMenu;
