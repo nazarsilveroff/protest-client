85fa91ad2a5ac57ec58d8a7d7e1e013f63e8b7a0
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2022-04-30 22:25:54 +0300

    TestPage

--- src/modules/TestForm/TestForm.jsx
+++ src/modules/TestForm/TestForm.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import s from "./TestForm.module.css";
 
-function TestForm(props) {
-    return (
-        <div></div>
-    );
+function TestForm() {
+  return (
+    <div className={s.containerTest}>
+      <p className={s.testQuestion}>
+        question <span className={s.testNumber}>3</span> / 12
+      </p>
+      <h2 className={s.testText}>What is regression testing?</h2>
+      <form>
+        <label className={s.testLabel}>
+          <input className={s.testInput} type="radio" name="answer" />
+          <p className={s.testAnswer}>
+            This is testing of the main functionality of the application
+          </p>
+        </label>
+      </form>
+    </div>
+  );
 }
 
 export default TestForm;
