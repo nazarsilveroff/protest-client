c73697907814977fcd0dbf8d79932dd93f703340
NazarLaptop <nazar91187@gmail.com>
2022-04-30 21:22:14 +0300

    edit AuthPage logic

---
+++ src/modules/AuthForm/validation/schema.js
@@ -0,0 +1,11 @@
+import * as Yup from "yup";
+
+export const signUpValidationSchema = Yup.object().shape({
+    username: Yup.string().required("Field is Required!"),
+    email: Yup.string().required("Field is Required!"),
+    password: Yup.string().required("Field is Required!"),
+});
+export const signInValidationSchema = Yup.object().shape({
+    email: Yup.string().required("Field is Required!"),
+    password: Yup.string().required("Field is Required!"),
+});
