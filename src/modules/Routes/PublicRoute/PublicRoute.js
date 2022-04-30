846dd2173b84fa08fe7bcb27018d0ef099423f8c
NazarLaptop <nazar91187@gmail.com>
2022-04-30 21:20:42 +0300

    add routes access

--- src/modules/Routes/PublicRoute/PublicRoute.js
+++ src/modules/Routes/PublicRoute/PublicRoute.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import {Outlet, Navigate} from "react-router-dom";
+import useAuth from "../../../shared/hooks/useAuth";
 
 function PublicRoute(props) {
-    // // const isLogin = useAuth();
-    //
-    // if (isLogin) {
-    //     return <Navigate to="/test"/>
-    // }
-    // return <Outlet/>
+    const isLogin = useAuth();
+
+    if (isLogin) {
+        return <Navigate to="/test"/>
+    }
+    return <Outlet/>
 }
 
 export default PublicRoute;
