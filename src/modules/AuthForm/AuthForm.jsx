ae47e995641a66806319c4505c9a2149916926ba
Vlad Krizhanivskyi <vlad.krizhanivskyi@gmail.com>
2022-05-01 19:09:13 +0300

    style

--- src/modules/AuthForm/AuthForm.jsx
+++ src/modules/AuthForm/AuthForm.jsx
@@ -1,62 +1,70 @@
-import React, {useEffect, useState} from 'react';
-import {Form, Formik} from "formik";
-import style from "../../pages/AuthPage/AuthPage.module.css";
+import React, { useEffect, useState } from "react";
+import { Form, Formik } from "formik";
+import s from "./AuthForm.module.css";
 import InputField from "../../shared/components/InputField";
 import AuthButtons from "./AuthButtons";
-import {signInValidationSchema, signUpValidationSchema} from "./validation/schema";
+import {
+  signInValidationSchema,
+  signUpValidationSchema,
+} from "./validation/schema";
 
 function AuthForm(props) {
-    const signUp = {
-        email: "",
-        password: "",
-        username: "",
-    }
-    const signIn = {
-        email: "",
-        password: "",
-    }
+  const signUp = {
+    email: "",
+    password: "",
+    username: "",
+  };
+  const signIn = {
+    email: "",
+    password: "",
+  };
 
-    const [activeBtn, setActiveBtn] = useState("signUp")
+  const [activeBtn, setActiveBtn] = useState("signUp");
 
-    const [inputValues, setInputValues] = useState(signUp)
+  const [inputValues, setInputValues] = useState(signUp);
 
-    useEffect(() => {
-        activeBtn === "signIn" ? setInputValues(signIn) : setInputValues(signUp)
-    }, [activeBtn])
-    return (
-        <Formik
-            enableReinitialize
-            validationSchema={activeBtn === "signUp" ? signUpValidationSchema : signInValidationSchema}
-            initialValues={inputValues}
-            onSubmit={(values) => {
+  useEffect(() => {
+    activeBtn === "signIn" ? setInputValues(signIn) : setInputValues(signUp);
+  }, [activeBtn]);
+  return (
+    <Formik
+      enableReinitialize
+      validationSchema={
+        activeBtn === "signUp" ? signUpValidationSchema : signInValidationSchema
+      }
+      initialValues={inputValues}
+      onSubmit={(values) => {}}
+    >
+      {({ isSubmitting }) => (
+        <Form className={s.form}>
+          {activeBtn === "signUp" ? (
+            <InputField
+              styles={{ input: s.input, label: s.label }}
+              type="text"
+              name="username"
+              placeholder="Nickname"
+            />
+          ) : null}
 
-            }}
-        >
-            {({isSubmitting}) => (
-                <Form className={style.form}>
+          <InputField
+            styles={{ input: s.input, label: s.label }}
+            type="email"
+            name="email"
+            placeholder="E-mail"
+          />
 
-                    {activeBtn === "signUp"
-                        ? <InputField styles={{input: style.input, label: style.label}}
-                                      type="text"
-                                      name="username"
-                                      placeholder="Nickname"/>
-                        : null}
+          <InputField
+            styles={{ input: s.input, label: s.label }}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
 
-                    <InputField styles={{input: style.input, label: style.label}}
-                                type="email" name="email"
-                                placeholder="E-mail"/>
-
-                    <InputField styles={{input: style.input, label: style.label}}
-                                type="password"
-                                name="password"
-                                placeholder="Password"/>
-
-                    <AuthButtons setActiveBtn={setActiveBtn} activeBtn={activeBtn} />
-
-                </Form>
-            )}
-        </Formik>
-    );
+          <AuthButtons setActiveBtn={setActiveBtn} activeBtn={activeBtn} />
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 export default AuthForm;
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./AuthPage.module.css";
+import s from "./AuthPage.module.css";
 import AuthForm from "../../modules/AuthForm";
 import GoogleAuth from "../../modules/AuthForm/GoogleAuth";
 
@@ -7,28 +7,28 @@ import GoogleAuth from "../../modules/AuthForm/GoogleAuth";
 function AuthPage(props) {
 
     return (
-        <main className={style.authMain}>
-            <div className={style.container}>
-                <section className={style.titleSection}>
-                    <h1 className={style.authTitle}>Pro Test</h1>
-                    <p className={style.authDescription}>
-                        <span className={style.boldSymbol}>&#91;</span> We will help you
+        <main className={s.authMain}>
+            <div className={s.container}>
+                <section className={s.titleSection}>
+                    <h1 className={s.authTitle}>Pro Test</h1>
+                    <p className={s.authDescription}>
+                        <span className={s.boldSymbol}>&#91;</span> We will help you
                         find weak points in knowledge so that you can strengthen it. We will
                         show you what is relevant to know for a
-                        <span className={style.boldSymbol}> QA Engineer</span> and will try
+                        <span className={s.boldSymbol}> QA Engineer</span> and will try
                         to make the learning process more diverse_
-                        <span className={style.boldSymbol}>&#93;</span>
+                        <span className={s.boldSymbol}>&#93;</span>
                     </p>
                 </section>
-                <section className={style.formSection}>
-                    <div className={style.formWrapper}>
-                        <p className={style.formDescription}>
+                <section className={s.formSection}>
+                    <div className={s.formWrapper}>
+                        <p className={s.formDescription}>
                             You can use your Google Account to authorize:
                         </p>
 
-                        <GoogleAuth styles={style.googleAuth}/>
+                        <GoogleAuth styles={s.googleAuth}/>
 
-                        <p className={style.formDescription}>
+                        <p className={s.formDescription}>
                             Or login to our using e-mail and password
                         </p>
 
