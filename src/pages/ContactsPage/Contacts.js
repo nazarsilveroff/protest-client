635da271119f5855cd7feebc5c2fbc73f213e73d
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2022-05-02 22:29:03 +0300

    Contacts

---
+++ src/pages/ContactsPage/Contacts.js
@@ -0,0 +1,59 @@
+import Nazar from "../../images/photo_Nazar.jpg";
+import Valeriia from "../../images/photo_Valeriia.jpg";
+import Svitlana from "../../images/photo_Svitlana.jpg";
+import Gabriel from "../../images/photo_Gabriel.jpg";
+import Vlad from "../../images/photo_Vlad.jpg";
+import Dmytro from "../../images/photo_Dmytro.jpg";
+import Katya from "../../images/photo_Katya.jpg";
+
+export const contacts =[
+  {
+    "id": "1",
+    "image": Nazar,
+    "name": "Nazar Pitylko",
+    "position": "Team Lead",
+    "github": "https://github.com/nazarsilveroff",
+  },
+  {
+    "id": "2",
+    "image": Valeriia,
+    "name": "Valeriia Savranska",
+    "position": "Scrum Master",
+    "github": "https://github.com/ValeriiaSavranska",
+  },
+  {
+    "id": "3",
+    "image": Svitlana,
+    "name": "Svitlana Onyschenko",
+    "position": "Front-End Developer",
+    "github": "https://github.com/Sv0707",
+  },
+  {
+    "id": "4",
+    "image": Gabriel,
+    "name": "Gabriel Revutskyi",
+    "position": "Front-End Developer",
+    "github": "https://github.com/revucky",
+  },
+  {
+    "id": "5",
+    "image": Vlad,
+    "name": "Vlad Krizhanivskyi",
+    "position": "Front-End Developer",
+    "github": "https://github.com/Subm1s",
+  },
+  {
+    "id": "6",
+    "image": Dmytro,
+    "name": "Dmytro Chuiko",
+    "position": "Front-End Developer",
+    "github": "https://github.com/Botsman9",
+  },
+  {
+    "id": "7",
+    "image": Katya,
+    "name": "Kateryna Vasylieva",
+    "position": "Front-End Developer",
+    "github": "https://github.com/CatarinaLeon",
+  }
+]
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import s from "./Contacts.module.css";
+import { contacts } from "./Contacts.js";
+import Icons from "../../images/icons.svg";
 
 function ContactsPage(props) {
-    return (
-        <div></div>
-    );
+  const [card] = useState(contacts);
+  return (
+    <section className={s.section}>
+      <div className={s.container}>
+        <h2 className={s.contactsTeam}>Out team</h2>
+        <ul className={s.contactsList}>
+          {card.map(({ id, name, position, image, github }) => (
+            <li className={s.contactsItem} key={id}>
+              <img className={s.contactsImg} src={image} alt="" />
+              <div className={s.contactsCard}>
+                <h3 className={s.contactsName}>{name}</h3>
+                <p className={s.contactsPosition}>{position}</p>
+                <a className={s.contactsLink} href={github} target="blank">
+                  <svg className={s.contactsSVG} width="30" height="30">
+                    <use xlinkHref={`${Icons}#icon-github`}></use>
+                  </svg>
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default ContactsPage;
